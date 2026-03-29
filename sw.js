838af86a803e83c0920a1c83d51398a22c65fab2
gpetrone80-lab <gpetrone80@gmail.com>
2026-03-29 17:31:08 -0400

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,46 +1,21 @@
-const CACHE_NAME = 'phfr-dashboard-v2';
+const CACHE_NAME = 'phfr-v2';
 const urlsToCache = [
   './',
   './index.html',
   './manifest.json',
-  './images/icons/icon-192x192.png',
-  './images/icons/icon-512x512.png'
+  './sw.js',
+  'images/icons/icon-192x192.png',
+  'images/icons/icon-512x512.png'
 ];
 
-// Install the Service Worker and cache essential files
 self.addEventListener('install', event => {
   event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => {
-      console.log('Opened cache');
-      return cache.addAll(urlsToCache);
-    })
+    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
   );
 });
 
-// Cache hit - return response, otherwise fetch from network
 self.addEventListener('fetch', event => {
   event.respondWith(
-    caches.match(event.request).then(response => {
-      if (response) {
-        return response;
-      }
-      return fetch(event.request);
-    })
-  );
-});
-
-// Activate and clean up old caches
-self.addEventListener('activate', event => {
-  const cacheWhitelist = [CACHE_NAME];
-  event.waitUntil(
-    caches.keys().then(cacheNames => {
-      return Promise.all(
-        cacheNames.map(cacheName => {
-          if (cacheWhitelist.indexOf(cacheName) === -1) {
-            return caches.delete(cacheName);
-          }
-        })
-      );
-    })
+    caches.match(event.request).then(response => response || fetch(event.request))
   );
 });
